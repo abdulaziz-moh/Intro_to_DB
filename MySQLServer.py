a15e7a99aee08880bb894c620e6b23fe194c1a99
abdulaziz-mohs <accoabdulazizmohammed@gmail.com>
2025-06-29 21:48:12 +0300

    task 0 and task 1 error correction

--- MySQLServer.py
+++ MySQLServer.py
@@ -7,12 +7,13 @@ try:
         user='your_mysql_username',
         password='your_mysql_password'
     )
-    cursor = conn.cursor()
-    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-    print("Database 'alx_book_store' created successfully!")
-except Error as e:
-    print(f"Error: {e}")
-finally:
     if conn.is_connected():
+        cursor = conn.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+finally:
+    if 'conn' in locals() and conn.is_connected():
         cursor.close()
         conn.close()
